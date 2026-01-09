e17342fefff021bcbb8a81b0fd8ac7580a682eaf
Victor Henrique Souza <vhs.desenvolvedorweb@gmail.com>
2026-01-08 23:03:23 -0300

    update 2

--- js/index.js
+++ js/index.js
@@ -1,5 +1,4 @@
 function openMenu() {
     console.log('O menu foi aberto.');
     document.querySelector('header').classList.toggle('open');
-    document.querySelector('.items').classList.toggle('open');
 };
